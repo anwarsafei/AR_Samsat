6ca140773e921626e2dd63066bd14dd479a93940
Anwar Safei <muhammad.anwar.safei@gmail.com>
2018-11-06 23:09:40 +0700

    Initial Project AR_Samsat

---
+++ Assets/Script/VideoMgr.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VideoMgr : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void OpenVideo(string url)
+	{
+		Handheld.PlayFullScreenMovie (url);
+	}
+}
